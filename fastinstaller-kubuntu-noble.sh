8a26f219aa0a45086b092eb5d6c486215ff4e644
Mario Loderer <mario.loderer@osit.cc>
2024-07-28 18:04:36 +0000

    disable apt-cacher per default

--- fastinstaller-kubuntu-noble.sh
+++ fastinstaller-kubuntu-noble.sh
@@ -9,7 +9,7 @@
 
 # Apt Proxy bearbeiten oder einkommentieren
 # Edit or comment Apt Proxy
-echo 'Acquire::http { Proxy "http://apt-cacher.osit.cc:3142"; };' | tee /etc/apt/apt.conf.d/01proxy
+#echo 'Acquire::http { Proxy "http://apt-cacher.osit.cc:3142"; };' | tee /etc/apt/apt.conf.d/01proxy
 
 gpg -k && gpg --no-default-keyring --keyring /usr/share/keyrings/iteas-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 23CAE45582EB0928
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/iteas-keyring.gpg] http://apt.iteas.at/iteas noble main" > /etc/apt/sources.list.d/iteas.list
